ee73c493ebe5fc79fa10b78f218d7d80291d57a8
Hankswsy <swsy0919@gmail.com>
2021-10-12 17:54:35 +0800

    first commit

---
+++ tellocontro.py
@@ -0,0 +1,9 @@
+import socket
+import time
+
+def notice():
+
+def 
+
+if __name__=="__main__":
+    while True
---
+++ tellotet.py
@@ -0,0 +1,31 @@
+import socket
+import time 
+
+host = ''
+port = 9000
+locaddr = (host,port)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tello_address = ('192.168.10.1',8889)
+sock.bind(locaddr)
+
+sock.sendto(b"command", tello_address)
+response, ip = sock.recvfrom(1024)
+print("command: ",response)
+
+sock.sendto(b"takeoff", tello_address)
+response, ip = sock.recvfrom(1024)
+print("takeoff: ",response)
+time.sleep(3)
+
+sock.sendto(b"battery?", tello_address)
+response, ip = sock.recvfrom(1024)
+print("battery? ",response)
+
+sock.sendto(b"land", tello_address)
+response, ip = sock.recvfrom(1024)
+print("land: ",response)
+
+sock.close()
+
+
+
